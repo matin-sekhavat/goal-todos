2cc20369ab0aa1f0a2e0d38e1e5fc88f516c3eab
matin-sekhavat <matinneural@gmail.com>
2024-04-02 22:46:44 +0330

    setup CourseGoal Component type and Create header Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import CourseGoal from "./components/CourseGoal";
+import Header from "./components/HEader";
 
 const App = () => {
   return (
     <main className="h-dvh flex justify-center bg-green-400 ">
       <div className="container max-w-screen-sm">
-        <CourseGoal title="FAKE TITLE" desc="FAKE DESC" />
-        <CourseGoal title="matin sekhavat" desc="test2" />
+        <Header />
+        <CourseGoal title="FAKE TITLE">test children</CourseGoal>
       </div>
     </main>
   );
--- src/components/CourseGoal.tsx
+++ src/components/CourseGoal.tsx
@@ -1,16 +1,16 @@
-import React from "react";
+import React, { PropsWithChildren, type ReactNode } from "react";
 
 type CourseGoalProps = {
   title: string;
-  desc: string;
+  children: PropsWithChildren<ReactNode>;
 };
 
-const CourseGoal: React.FC<CourseGoalProps> = ({ title, desc }) => {
+const CourseGoal: React.FC<CourseGoalProps> = ({ title, children }) => {
   return (
     <article className="bg-slate-800 text-white p-4 rounded-lg mt-4">
       <div>
         <h2>{title}</h2>
-        <p>{desc}</p>
+        <p>{children}</p>
       </div>
       <button className="bg-stone-400 rounded-md p-2">Delete</button>
     </article>
@@ -18,3 +18,7 @@ const CourseGoal: React.FC<CourseGoalProps> = ({ title, desc }) => {
 };
 
 export default CourseGoal;
+// for using children cause the wrapp element is tag you can use ReactNode type
+// ReactNode  |  PropsWithChildren<ReactNode>
+// PropsWithChildren is a genericType should be include <>
+// Look FC is Also Generic Type  React.FC<یچی>
